f5cc8dfd6510110a295670540a43a96f90fa067f
OughtNaut <112574273+OughtNaut@users.noreply.github.com>
2023-02-08 21:15:22 -0700

    added more functionality to the demo app. including mutiple groups as well as a simple car model.

--- danshadybusiness/demo/views.py
+++ danshadybusiness/demo/views.py
@@ -1,7 +1,10 @@
 from django.http import HttpResponse
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
 from django.contrib import messages
+from django.urls import reverse
+from .models import Car
 
 
 # Create your views here.
@@ -13,9 +16,29 @@ def index(request):
         user = authenticate(request, username=username, password=password)
         if user is not None:
             login(request,user)
-            return redirect("Logged_In")
+            return redirect(reverse('demo:Logged_In'))
         else:
             return render(request, 'authenticate/login.html')
     return render(request, 'authenticate/login.html')
+
+@login_required
 def loggedIn(request):
     return render(request, 'authenticate/logged_in.html')
+
+
+@login_required
+def cars(request):
+    carTotal = Car.objects.all().count()
+    context = {
+        'total': carTotal,
+    }
+    return render(request, 'authenticate/cars.html', context)
+
+def addCar(request):
+    c = Car(color='red')
+    c.save()
+    return redirect(reverse('demo:cars'))
+
+def logoutView(request):
+    logout(request)
+    return redirect(reverse('demo:Login'))
