aaac12f012fd29ce0e16883086ba6d9508a3ae76
Michael Hanks <mhanks445@yahoo.com>
2023-02-14 17:54:44 -0700

    Add Custom User model

--- danshadybusiness/product/models.py
+++ danshadybusiness/product/models.py
@@ -1,3 +1,36 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
-# Create your models here.
+
+
+
+class CustomUser(models.Model):
+    user = models.OneToOneField(User,on_delete=models.CASCADE)
+    balance = models.FloatField(default = 0.0)
+    hours = models.FloatField(default = 0.0)
+
+    def addFunds(self,amount):
+        self.balance += amount
+    
+    def addHours(self,amount):
+        self.hours += amount
+    
+    
+
+    
+
+#this method is to create CustomUser when User is created
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        CustomUser.objects.create(user=instance)
+
+#this method to update profile when user is updated
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
+    instance.customuser.save()
+
+
+
--- danshadybusiness/product/views.py
+++ danshadybusiness/product/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
+from product.models import CustomUser
 
 def addFunds(request):
+    
     return render(request, 'product/addFunds.html', {})
 
 # Create your views here.
