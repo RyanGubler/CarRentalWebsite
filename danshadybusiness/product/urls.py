c190f08e8437ff5744a58b284d51cbdfcae076bd
Kaiden McMillen <a02360946@aggies.usu.edu>
2023-02-16 13:36:39 -0700

    added skeleton for login, signup, and serviceticket templates, added them to urls.py, and gave them basic views

--- danshadybusiness/product/urls.py
+++ danshadybusiness/product/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns = [
     path('addFunds/', views.addFunds, name='addFunds'),
+    path('login/', views.login, name='login'),
+    path('signup/', views.signup, name='signup'),
+    path('service/', views.service, name='service'),
 ]
--- danshadybusiness/product/views.py
+++ danshadybusiness/product/views.py
@@ -3,4 +3,12 @@ from django.shortcuts import render
 def addFunds(request):
     return render(request, 'product/addFunds.html', {})
 
+def login(request):
+    return render(request, 'product/login.html', {})
+
+def signup(request):
+    return render(request, 'product/signup.html', {})
+
+def service(request):
+    return render(request, 'product/serviceTicket.html', {})
 # Create your views here.
