3f1d2957312d6bd0ad294d7ca444fd93f6f256af
ryangoobs24 <ryan24gubler@gmail.com>
2023-02-17 12:22:03 -0700

    created header footer and addFunds html pages along with the styling

--- danshadybusiness/danshadybusiness/settings.py
+++ danshadybusiness/danshadybusiness/settings.py
@@ -11,10 +11,8 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
-
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
@@ -124,4 +122,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
 
