2a4169086ef8e698f809cc22d5e16b8a45e10f00
ryangoobs24 <ryan24gubler@gmail.com>
2023-02-17 12:38:02 -0700

    made a few more changes for the next page

--- danshadybusiness/product/urls.py
+++ danshadybusiness/product/urls.py
@@ -5,4 +5,5 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('addFunds/', views.addFunds, name='addFunds'),
+    path('aboutUs/', views.aboutUs, name='aboutUs'),
 ]
--- danshadybusiness/product/views.py
+++ danshadybusiness/product/views.py
@@ -3,4 +3,7 @@ from django.shortcuts import render
 def addFunds(request):
     return render(request, 'product/addFunds.html', {})
 
+def aboutUs(request):
+    return render(request, 'product/aboutUs.html', {})
+
 # Create your views here.
