b980499d076aa37721c3f2f21a981fc1729748e5
OughtNaut <112574273+OughtNaut@users.noreply.github.com>
2023-02-18 12:20:12 -0700

    Built custom permissions for each group into the migration.

--- danshadybusiness/product/migrations/0001_initial.py
+++ danshadybusiness/product/migrations/0001_initial.py
@@ -2,12 +2,16 @@
 
 from django.db import migrations
 from django.contrib.auth.management import create_permissions
-from django.contrib.auth.models import Group, Permission
+from django.contrib.auth.models import Group, Permission, User
+from django.contrib.contenttypes.models import ContentType
 
 def createGroups(apps, schema_editor):
+    contentType = ContentType.objects.get_for_model(User)
     groups = ["User", "Employee", "Manager"]
     for groupName in groups:
-        Group.objects.create(name=groupName)
+        permission = Permission.objects.create(codename=groupName, name=groupName, content_type= contentType)
+        newGroup =   Group.objectscreate(name=groupName)
+        newGroup.permissions.add(permission)
 
 class Migration(migrations.Migration):
 
