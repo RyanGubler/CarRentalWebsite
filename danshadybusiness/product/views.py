03e37bbed6b960c518515b39ddf16b845380e5b1
OughtNaut <112574273+OughtNaut@users.noreply.github.com>
2023-02-20 17:45:18 -0700

    added a createuser view

--- danshadybusiness/product/views.py
+++ danshadybusiness/product/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from django.contrib.auth.models import Group, Permission, User
+from django.contrib.auth import authenticate, login
 from .models import ServiceTicket
 def addFunds(request):
     return render(request, 'product/addFunds.html', {})
@@ -18,3 +20,13 @@ def terminate(request):
 def service(request):
     ticketList = list[ServiceTicket.objects.all()[:5]]
     return render(request, 'product/serviceTicket.html', {'ticketList' : ticketList})
+
+def signup(request):
+    if request.method == 'POST':
+        user = User.objects.create_user(username = request.POST['username'],
+                                        password = request.POST['password'], 
+                                        email=request.POST['email'], 
+                                        firstname = request.POST['firstName'], 
+                                        lastname = request.POST['lastName'])
+        return(request, 'product/login.html', {})
+    return render(request,'product/signup.html', {})
