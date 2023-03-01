90f9f6351a5446a125efd64af82ddf286e993b75
ryangoobs24 <ryan24gubler@gmail.com>
2023-03-01 08:26:17 -0700

    git the front end for the reservation set up

--- danshadybusiness/product/static/product/js/calendar.js
+++ danshadybusiness/product/static/product/js/calendar.js
@@ -7,9 +7,9 @@ window.onload = function() {
     function pikadayCalender(){
         startDatePicker = new Pikaday({ field: document.getElementById('start-date'),
                                         minDate: moment().toDate(),
-                                        format: 'MM/DD/YYYY'});
+                                        format: 'YYYY/MM/DD'});
         endDatePicker = new Pikaday({ field: document.getElementById('end-date'),
-                                        format: 'MM/DD/YYYY'});
+                                        format: 'YYYY/MM/DD'});
         startDatePicker.setMinDate(new Date());
         endDatePicker.setMinDate(new Date())
         startDatePicker._o.onSelect = function(date){
